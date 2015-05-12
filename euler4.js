2abd036aad8a1e4bac03e7ef1f4d034dbb41563f
Jenny Zurawell <jenny@ted.com>
2015-05-12 11:53:03 -0700

    solution to #4

---
+++ euler4.js
@@ -0,0 +1,17 @@
+var palindrome = [];
+
+for (var i = 999; i > 99; i--) {
+    for (var j = 999; j > 99; j--) {
+        var product = (i * j);
+        var productString = product.toString();
+        var backwardString = productString.split("").reverse().join("");
+        
+        if (productString === backwardString) {
+             palindrome.push(product);
+        }
+    }
+}
+
+var max_of_array = Math.max.apply(Math, palindrome);
+console.log(max_of_array);
+
