3ada3211ccff82f0eec75398c51137a1e32250f4
Jenny Zurawell <jenny@ted.com>
2015-05-13 19:25:37 -0700

    numero 5

--- euler5.js
+++ euler5.js
@@ -1,4 +1,19 @@
-for (var x = 2; x < 21; x++) {
+var factorial = 1;
 
+for (var x = 1; x < 21; x++) {
+    factorial = x * factorial;
 }
- 
+
+for (var n = 20; n < factorial; n += 20) {
+    var isDivisible = true;
+    for (var x = 1; x < 21; x++) {
+        if (n % x !== 0) {
+            isDivisible = false;
+            break;
+        }
+    }
+    if (isDivisible) {
+        console.log(n);
+        break;
+    }
+}
