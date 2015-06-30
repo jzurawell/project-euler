a4a20a518451e9ad95c0648383b9917a643378a1
Jenny Zurawell <jenny@ted.com>
2015-06-30 14:06:23 -0700

    euler 9 solution

---
+++ euler9.js
@@ -0,0 +1,21 @@
+function pythagorean(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+for (var a = 1; a < 1000; a++) {
+    for (var b = 2; b < 1000; b++) {
+        for (var c = 3; c < 1000; c++) {
+            if (a < b) {
+                if (b < c) {
+                    if (a + b + c === 1000) {
+                        if ((a * a) + (b * b) === (c * c)) {
+                            pythagorean(a, b, c);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
